1cab062d8190366289f6731b467a5dcf1da5c973
Tella Abdulrasheed <rashtell2@gmail.com>
2022-10-11 23:56:20 +0100

    handle exceptions with the custom exception handler

--- src/main/java/com/rashtell/demo/student/StudentService.java
+++ src/main/java/com/rashtell/demo/student/StudentService.java
@@ -1,16 +1,13 @@
 package com.rashtell.demo.student;
 
+import com.rashtell.demo.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.io.InputStream;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Scanner;
 
 @Service
 public class StudentService {
@@ -27,10 +24,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-      Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
 
-        if (studentByEmail.isPresent()){
-            throw new IllegalStateException("Email taken");
+        if (studentByEmail.isPresent()) {
+//            throw new IllegalStateException("Email taken");
+            throw new ApiRequestException("Email taken");
         }
 
         studentRepository.save(student);
@@ -40,8 +38,9 @@ public class StudentService {
     public void deleteStudent(Long studentId) {
         boolean studentExtsts = studentRepository.existsById(studentId);
 
-        if(!studentExtsts){
-            throw new IllegalStateException("Student with id "+studentId+" does not exist");
+        if (!studentExtsts) {
+//            throw new IllegalStateException("Student with id "+studentId+" does not exist");
+            throw new ApiRequestException("Student with id " + studentId + " does not exist");
         }
 
         studentRepository.deleteById(studentId);
@@ -50,23 +49,25 @@ public class StudentService {
     @Transactional
     public void updateStudent(Long studentId, String name, String email) {
         Student student = studentRepository.findById(studentId)
-                .orElseThrow(()->new IllegalStateException(
-                        "Student with id "+ studentId + "does not exist"));
+                .orElseThrow(() -> new ApiRequestException(
+                        "Student with id " + studentId + "does not exist"));
+//                        new IllegalStateException("Student with id "+ studentId + "does not exist"));
 
         if (name != null &&
                 name.length() > 0 &&
-                !Objects.equals(student.getName(), name)){
+                !Objects.equals(student.getName(), name)) {
             student.setName(name);
         }
 
         if (email != null &&
                 email.length() > 0 &&
-                !Objects.equals(student.getEmail(), email)){
+                !Objects.equals(student.getEmail(), email)) {
 
             Optional<Student> studentEmail = studentRepository.findStudentByEmail(email);
 
-            if (studentEmail.isPresent()){
-                throw new IllegalStateException("Email taken");
+            if (studentEmail.isPresent()) {
+//                throw new IllegalStateException("Email taken");
+                throw new ApiRequestException("Email taken");
             }
 
             student.setEmail(email);
